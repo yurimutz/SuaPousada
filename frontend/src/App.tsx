cba3d3d65eac759228a6c6243e6ef819ec8563a6
RafaBaker <rafael1309mt@gmail.com>
2026-06-19 18:20:15 -0300

    fix: modularização e conteinerização

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+
+export function App() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div>
+          <h1 className="font-bold">Essa página está em construção</h1>
+          {/* <p>You may now add components and start building.</p>
+          <p>We&apos;ve already added the button component for you.</p> */}
+          {/* <Button className="mt-2">Button</Button> */}
+        </div>
+        <div className="font-mono text-xs text-muted-foreground">
+          (Porém, aperte <kbd>d</kbd> para mudar o tema 😏) 
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
