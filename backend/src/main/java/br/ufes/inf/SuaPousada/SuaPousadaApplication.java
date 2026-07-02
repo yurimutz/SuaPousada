603c8306f42b8233acc0903cd125ed70cd31c00f
brvale05 <brvale05@gmail.com>
2026-07-02 07:15:47 -0300

    save

--- backend/src/main/java/br/ufes/inf/SuaPousada/SuaPousadaApplication.java
+++ backend/src/main/java/br/ufes/inf/SuaPousada/SuaPousadaApplication.java
@@ -13,8 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuaPousadaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SuaPousadaApplication.class, args);
+
+
 	}
 
 }
