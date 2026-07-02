866f68cd514600e9bb88ad65dcf97b32bc5a64ed
RafaBaker <rafael1309mt@gmail.com>
2026-07-01 22:15:23 -0300

    fix: persistencia no banco de dados docker

--- backend/src/main/java/br/ufes/inf/SuaPousada/SuaPousadaApplication.java
+++ backend/src/main/java/br/ufes/inf/SuaPousada/SuaPousadaApplication.java
@@ -1,7 +1,12 @@
 package br.ufes.inf.SuaPousada;
 
+import br.ufes.inf.SuaPousada.domain.Genero;
+import br.ufes.inf.SuaPousada.dto.request.FuncionarioCreateRequestDTO;
+import br.ufes.inf.SuaPousada.service.FuncionarioService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 
 /** 
