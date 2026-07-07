829b5bd9fb43be0d7b1782a692ed64253c1abd66
RafaBaker <rafael1309mt@gmail.com>
2026-07-07 12:32:52 -0300

    feat: formulario de criação de hóspedes validado

--- frontend/src/components/forms/hospede-form.tsx
+++ frontend/src/components/forms/hospede-form.tsx
@@ -1,8 +1,17 @@
 import { Button } from "@/components/ui/button";
-import { DialogFooter } from "@/components/ui/dialog";
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Field, FieldContent, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
 import { type Hospede } from "../data-table/hospedes-columns";
 import { DatePickerSimple } from "../date-picker-birthday";
 
@@ -11,32 +20,52 @@ interface HospedeFormProps {
     onSuccess: () => void;
 }
 
+const createHospedeFormSchema = z.object({
+    nome: z.string().nonempty("O nome é obrigatório").max(80, "Máximo de 80 caracteres"),
+    cpf: z.string().min(11, "CPF deve ter no mínimo 11 números").max(14, "CPF inválido"),
+    telefone: z.string().min(10, "Telefone inválido").max(11, "Telefone muito grande"),
+    dtNascimento: z.string().nonempty("Data de nascimento é obrigatória"),
+    genero: z.string().min(1, "Gênero é obrigatório"),
+    email: z.email("E-mail inválido"),
+});
+
+type HospedeFormValues = z.infer<typeof createHospedeFormSchema>;
+
 export function HospedeForm({ hospede, onSuccess }: HospedeFormProps) {
     const isEditing = !!hospede;
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        
-        const formData = new FormData(e.currentTarget);
-        const data = Object.fromEntries(formData.entries());
+    const form = useForm<HospedeFormValues>({
+        resolver: zodResolver(createHospedeFormSchema),
+        defaultValues: {
+            nome: hospede?.nome || "",
+            cpf: hospede?.cpf || "",
+            telefone: hospede?.telefone || "",
+            dtNascimento: hospede?.dtNascimento || "",
+            genero: hospede?.genero || "",
+            email: hospede?.email || "",
+        }
+    });
+
+    const onSubmit = (data: HospedeFormValues) => {
+        console.log("🚀 Payload sendo enviado para a API:", data);
 
         if (isEditing) {
             axios.patch(`http://localhost:8080/cliente/${hospede.id}/update`, data)
                 .then((response) => {
-                    console.log("Update feito com sucesso!", response.data);
+                    console.log("✅ Update feito com sucesso!", response.data);
                     onSuccess();
                 })
                 .catch((error) => {
-                    console.log("Erro ao atualizar", error);
+                    console.error("❌ Erro ao atualizar. Detalhes:", error.response?.data || error);
                 });
         } else {
             axios.post(`http://localhost:8080/cliente/create`, data)
                 .then((response) => {
-                    console.log("Criado com sucesso!", response.data);
+                    console.log("✅ Criado com sucesso!", response.data);
                     onSuccess();
                 })
                 .catch((error) => {
-                    console.log("Erro ao criar", error);
+                    console.error("❌ Erro ao criar. Detalhes:", error.response?.data || error);
                 });
         }
     };
@@ -44,38 +73,148 @@ export function HospedeForm({ hospede, onSuccess }: HospedeFormProps) {
     const idPrefix = isEditing ? hospede.id : "new";
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
             <FieldGroup>
-                <Field>
-                    <FieldLabel htmlFor={`nome-${idPrefix}`}>Nome</FieldLabel>
-                    <Input id={`nome-${idPrefix}`} name="nome" defaultValue={hospede?.nome} required />
-                </Field>
-                <Field>
-                    <FieldLabel htmlFor={`cpf-${idPrefix}`}>CPF</FieldLabel>
-                    <Input id={`cpf-${idPrefix}`} name="cpf" defaultValue={hospede?.cpf} required />
-                </Field>
-                <Field>
-                    <FieldLabel htmlFor={`telefone-${idPrefix}`}>Telefone</FieldLabel>
-                    <Input id={`telefone-${idPrefix}`} name="telefone" defaultValue={hospede?.telefone} required />
-                </Field>
-                <DatePickerSimple 
-                    id={`nasc-${idPrefix}`} 
-                    name="dtNascimento" 
-                    defaultValue={hospede?.dtNascimento} 
-                    required 
+                <Controller
+                    name="nome"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                        <Field data-invalid={fieldState.invalid}>
+                            <FieldLabel htmlFor={`nome-${idPrefix}`}>Nome</FieldLabel>
+                            <Input
+                                {...field}
+                                id={`nome-${idPrefix}`}
+                                type="text"
+                                aria-invalid={fieldState.invalid}
+                            />
+                            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                        </Field>
+                    )}
                 />
-                <Field>
-                    <FieldLabel htmlFor={`genero-${idPrefix}`}>Gênero</FieldLabel>
-                    <Input id={`genero-${idPrefix}`} name="genero" defaultValue={hospede?.genero} required />
-                </Field>
-                <Field>
-                    <FieldLabel htmlFor={`email-${idPrefix}`}>E-mail</FieldLabel>
-                    <Input id={`email-${idPrefix}`} name="email" type="email" defaultValue={hospede?.email} required />
-                </Field>
+
+                <Controller
+                    name="cpf"
+                    control={form.control}
+                    render={({ field: { onChange, value, ...rest }, fieldState }) => {
+                        const displayValue = value
+                            .replace(/\D/g, '')
+                            .replace(/(\d{3})(\d)/, '$1.$2')
+                            .replace(/(\d{3})(\d)/, '$1.$2')
+                            .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+                        return (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={`cpf-${idPrefix}`}>CPF</FieldLabel>
+                                <Input
+                                    {...rest}
+                                    value={displayValue}
+                                    onChange={(e) => {
+                                        onChange(e.target.value.replace(/\D/g, ''));
+                                    }}
+                                    id={`cpf-${idPrefix}`}
+                                    type="text"
+                                    maxLength={14}
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )
+                    }}
+                />
+
+                <Controller
+                    name="telefone"
+                    control={form.control}
+                    render={({ field: { onChange, value, ...rest }, fieldState }) => {
+                        const displayValue = value
+                            .replace(/\D/g, '')
+                            .replace(/(\d{2})(\d)/, '($1) $2')
+                            .replace(/(\d{5})(\d{1,4})$/, '$1-$2');
+
+                        return (
+                            <Field data-invalid={fieldState.invalid}>
+                                <FieldLabel htmlFor={`telefone-${idPrefix}`}>Telefone</FieldLabel>
+                                <Input
+                                    {...rest}
+                                    value={displayValue}
+                                    onChange={(e) => {
+                                        onChange(e.target.value.replace(/\D/g, ''));
+                                    }}
+                                    id={`telefone-${idPrefix}`}
+                                    type="text"
+                                    maxLength={15}
+                                    aria-invalid={fieldState.invalid}
+                                />
+                                {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            </Field>
+                        )
+                    }}
+                />
+
+                <Controller
+                    name="dtNascimento"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                        <Field data-invalid={fieldState.invalid}>
+                            <DatePickerSimple
+                                id={`nasc-${idPrefix}`}
+                                name={field.name}
+                                value={field.value}
+                                onChange={field.onChange}
+                            />
+                            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                        </Field>
+                    )}
+                />
+
+                <Controller
+                    name="genero"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                        <Field orientation="responsive" data-invalid={fieldState.invalid}>
+                            <FieldContent>
+                                <FieldLabel htmlFor={`genero-${idPrefix}`}>Gênero</FieldLabel>
+                            </FieldContent>
+                            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                            <Select 
+                                onValueChange={field.onChange}
+                                value={field.value || ''}
+                            >
+                                <SelectTrigger id={`genero-${idPrefix}`} aria-invalid={fieldState.invalid} className="min-w-30">
+                                    <SelectValue placeholder="Selecione..." />
+                                </SelectTrigger>
+                                <SelectContent position="item-aligned">
+                                    <SelectItem value="MASCULINO">Masculino</SelectItem>
+                                    <SelectItem value="FEMININO">Feminino</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </Field>
+                    )}
+                />
+
+                <Controller
+                    name="email"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                        <Field data-invalid={fieldState.invalid}>
+                            <FieldLabel htmlFor={`email-${idPrefix}`}>E-mail</FieldLabel>
+                            <Input
+                                {...field}
+                                id={`email-${idPrefix}`}
+                                type="email"
+                                aria-invalid={fieldState.invalid}
+                            />
+                            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                        </Field>
+                    )}
+                />
             </FieldGroup>
-            <DialogFooter className="mt-4">
-                <Button type="submit">{isEditing ? "Salvar alterações" : "Adicionar Hóspede"}</Button>
-            </DialogFooter>
+            
+            <div className="mt-6 flex flex-col sm:flex-row sm:justify-end">
+                <Button type="submit" className="w-full sm:w-auto">
+                    {isEditing ? "Salvar alterações" : "Adicionar Hóspede"}
+                </Button>
+            </div>
         </form>
     );
 }
