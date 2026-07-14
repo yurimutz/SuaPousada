2dbdad2a64fa26ee72814aa422ec292118970fe5
RafaBaker <rafael1309mt@gmail.com>
2026-07-14 14:59:47 -0300

    fix: correção no Seeder para gerar dados mais interessantes

--- backend/src/main/java/br/ufes/inf/SuaPousada/config/DataSeeder.java
+++ backend/src/main/java/br/ufes/inf/SuaPousada/config/DataSeeder.java
@@ -54,7 +54,8 @@ public class DataSeeder implements CommandLineRunner {
                         LocalDate.of(faker.number().numberBetween(1970, 2000), faker.number().numberBetween(1, 12), faker.number().numberBetween(1, 28)),
                         Genero.values()[random.nextInt(Genero.values().length)],
                         faker.internet().emailAddress(),
-                        faker.phoneNumber().cellPhone().replaceAll("[^0-9]", "").substring(0, 11)
+                        faker.phoneNumber().cellPhone().replaceAll("[^0-9]", "").substring(0, 11),
+                        "123456"
                 ));
             } catch (Exception e) {
                 System.out.println("ERRO ao criar funcionário: " + e.getMessage());
@@ -71,7 +72,8 @@ public class DataSeeder implements CommandLineRunner {
                         LocalDate.of(faker.number().numberBetween(1970, 2004), faker.number().numberBetween(1, 12), faker.number().numberBetween(1, 28)),
                         Genero.values()[random.nextInt(Genero.values().length)],
                         faker.internet().emailAddress(),
-                        faker.phoneNumber().cellPhone().replaceAll("[^0-9]", "").substring(0, 11)
+                        faker.phoneNumber().cellPhone().replaceAll("[^0-9]", "").substring(0, 11),
+                        "123456"
                 ));
                 clientes.add(cliente);
             } catch (Exception e) { System.out.println("ERRO ao criar cliente: " + e.getMessage()); }
@@ -82,33 +84,33 @@ public class DataSeeder implements CommandLineRunner {
         TipoQuartoResponseDTO tqMedio = tipoQuartoService.create(new TipoQuartoCreateRequestDTO("Suíte Master", 1, 1, 1, 350.0, true));
         TipoQuartoResponseDTO tqAlto = tipoQuartoService.create(new TipoQuartoCreateRequestDTO("Suíte Presidencial", 0, 2, 2, 850.0, true));
 
-        // 4. Criar 30 Quartos (Maior ocorrência dos baratos)
+        // 4. Criar 10 Quartos (Maior ocorrência dos baratos)
         List<QuartoResponseDTO> quartos = new ArrayList<>();
         int quartoNumero = 101;
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 10; i++) {
             Long tipoQuartoId;
-            if (i < 18) tipoQuartoId = tqBaixo.id();        // 18 Standard
-            else if (i < 26) tipoQuartoId = tqMedio.id();   // 8 Master
-            else tipoQuartoId = tqAlto.id();                // 4 Presidential
+            if (i < 5) tipoQuartoId = tqBaixo.id();        // 5 Standard
+            else if (i < 8) tipoQuartoId = tqMedio.id();   // 3 Master
+            else tipoQuartoId = tqAlto.id();                // 2 Presidential
 
             QuartoResponseDTO quarto = quartoService.create(new QuartoCreateRequestDTO(
                     quartoNumero++,
-                    i < 15 ? 1 : 2, // 1º e 2º Andar
+                    i < 5 ? 1 : 2, // 1º e 2º Andar
                     tipoQuartoId
             ));
             quartos.add(quarto);
         }
 
-        // 5. Criar Reservas para Clientes (1 a 3 por cliente)
+        // 5. Criar Reservas para Clientes (10 a 20 por cliente)
         for (ClienteResponseDTO cliente : clientes) {
-            int qtdReservas = random.nextInt(3) + 1; // 1 a 3 reservas
+            int qtdReservas = random.nextInt(11) + 10; // 10 a 20 reservas
 
             for (int j = 0; j < qtdReservas; j++) {
                 boolean sucesso = false;
                 int tentativas = 0;
 
                 // Tenta agendar uma data. Se der conflito (lançada pelo Service), tenta outra.
-                while (!sucesso && tentativas < 10) {
+                while (!sucesso && tentativas < 30) {
                     try {
                         // Data aleatória neste ano atual (de hoje até 6 meses para frente)
                         LocalDate checkIn = LocalDate.now().plusDays(random.nextInt(180) - 30); // De -30 dias atrás até 150 no futuro
